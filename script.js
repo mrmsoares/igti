51eb3dcf235e58ed9fb391a4a9bb8a7100a13381
Marcos Soares <marcosroberto.ms@gmail.com>
2021-03-20 17:24:15 -0300

    Adicionando várias soluções para as promises.

--- script.js
+++ script.js
@@ -1,20 +1,81 @@
-let employeesPromise = fetch("http://localhost:3000/employees");
+function solution1() {
+  let employeesPromise = fetch("http://localhost:3000/employees");
 
-employeesPromise.then((resp) => {
-  resp.json().then((employees) => {
-    // crio uma variável table e atribuo a ela a função renderTable, que está pegando todos os items id e name da lista.
-    let table = renderTable(employees);
+  employeesPromise.then((respEmployee) => {
+    respEmployee.json().then((employees) => {
+      // crio uma variável rolesPromise para captura dados das funções de cada funcionário na lista roles.
+      let rolesPromise = fetch("http://localhost:3000/roles");
 
-    // busco no documento html o id "app" e insiro dentro dele a tabela renderizada.
+      rolesPromise.then((respRoles) => {
+        respRoles.json().then((roles) => {
+          // crio uma variável table e atribuo a ela a função renderTable, que está pegando todos os items id e name da lista "employees" e o name da lista "roles".
+          let table = renderTable(employees, roles);
+
+          // busco no documento html o id "app" e insiro dentro dele a tabela renderizada.
+          document.getElementById("app").innerHTML = table;
+        });
+      });
+    });
+  });
+}
+//solution1();
+
+function solution2() {
+  fetch("http://localhost:3000/employees")
+    .then((respEmployee) => {
+      return respEmployee.json();
+    })
+    .then((employees) => {
+      fetch("http://localhost:3000/roles")
+        .then((respRoles) => {
+          return respRoles.json();
+        })
+        .then((roles) => {
+          let table = renderTable(employees, roles);
+          document.getElementById("app").innerHTML = table;
+        });
+    });
+}
+//solution2();
+
+function fetchJson(url) {
+  return fetch(url).then((r) => {
+    return r.json();
+  });
+}
+
+function solution3() {
+  fetchJson("http://localhost:3000/employees").then((employees) => {
+    fetchJson("http://localhost:3000/roles").then((roles) => {
+      let table = renderTable(employees, roles);
+      document.getElementById("app").innerHTML = table;
+    });
+  });
+}
+
+//solution3();
+
+function solution4() {
+  Promise.all([
+    fetchJson("http://localhost:3000/employees"),
+    fetchJson("http://localhost:3000/roles"),
+  ]).then(([employees, roles]) => {
+    let table = renderTable(employees, roles);
     document.getElementById("app").innerHTML = table;
   });
-});
+}
 
-function renderTable(employees) {
+solution4();
+
+function renderTable(employees, roles) {
+  //O método map() invoca a função callback passada por argumento para cada elemento do Array e devolve um novo Array como resultado.
   let rows = employees.map((employee) => {
+    //O método find() retorna o valor do primeiro elemento do array que satisfizer a função de teste provida.
+    let role = roles.find((role) => role.id == employee.role_id);
     return `<tr>
       <td>${employee.id}</td>
       <td>${employee.name}</td>
+      <td>${role.name}</td>
       </tr>`;
   });
   // O método join() junta todos os elementos de um array (ou um array-like object) em uma string e retorna esta string.

9bbbd9b163ae26f9f33f306c2d87c522589d20b2
Marcos Soares <marcosroberto.ms@gmail.com>
2021-03-29 21:35:47 -0300

    Configuração ações do usuário.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let employees = [];
 let roles = [];
 
 // criando variável para armazenar dados do clique na li
-let itemSelected;
+let selectedItem;
 
 const elementList = document.querySelector("ul");
 const elementForm = document.querySelector("form");
@@ -10,7 +10,7 @@ const elementForm = document.querySelector("form");
 // Capturando os botões delete, cancel e create
 const bdelete = document.getElementById("bdelete");
 const bcancel = document.getElementById("bcancel");
-const bcreate = document.getElementById("bcreate");
+const bsubmit = document.getElementById("bsubmit");
 
 async function init() {
   try {
@@ -21,8 +21,13 @@ async function init() {
     clearSelection();
     // Ao clicar no botão cancel ele limpa todos os campos e a seleção.
     bcancel.addEventListener("click", clearSelection);
+    // Ao clicar no botão update
+    elementForm.addEventListener("submit", onSubmit);
+
+    // ao clicar no botão delete
+    bdelete.addEventListener("click", onDelete);
   } catch (erro) {
-    showError(erro);
+    showError("Error loading data.", erro);
   }
 }
 init();
@@ -30,7 +35,7 @@ init();
 function selectItem(employee, li) {
   // chamamos a função clearSelection para limpar o item que esteja marcado com a classe selected antes de adicionar a classe a um novo item.
   clearSelection();
-  itemSelected = employee;
+  selectedItem = employee;
   li.classList.add("selected");
 
   // Capturando os dados do clique na li para aparecer dentro dos inputs (name, salary e role). Ao clicar pegamos os dados do clique e jogamos dentro do input utilizando o campo name de cada input.
@@ -41,11 +46,13 @@ function selectItem(employee, li) {
   // Ao clicar no selecionar algum item da lista os botões delete e cancel aparecem.
   bcancel.style.display = "inline";
   bdelete.style.display = "inline";
+  bsubmit.textContent = "Update";
 }
 
 // Criamos a função para remover a classe de estilo do item que estava clicado ao clicar em outro item.
 function clearSelection() {
-  itemSelected = undefined;
+  clearError();
+  selectedItem = undefined;
   // Busca dentro da UL qualquer elemento que esteja com a classe selected adicionada.
   const li = elementList.querySelector(".selected");
 
@@ -55,11 +62,65 @@ function clearSelection() {
   }
 
   // Ao clicar no botão cancel, os botões delete e cancel são ocultados
+  elementForm.name.value = "";
+  elementForm.salary.value = "";
+  elementForm.role_id.value = "";
   bcancel.style.display = "none";
   bdelete.style.display = "none";
+  bsubmit.textContent = "Create";
+}
+
+// Função para tratar os dados de atualização da lista
+async function onSubmit(evt) {
+  // Precisamos cancelar o comportamento padrão (carregar página) do submit atraves do preventDedault
+  evt.preventDefault();
+
+  const employeeData = {
+    name: elementForm.name.value,
+    salary: elementForm.salary.valueAsNumber,
+    // adicionando o operador + você transforma em number
+    role_id: +elementForm.role_id.value,
+  };
+
+  // Verificando se os campos estão vazios para não submeter o formulário
+  if (!employeeData.name || !employeeData.salary || !employeeData.role_id) {
+    showError("You must fill all form fields");
+  } else {
+    if (selectedItem) {
+      // Passamos a função updateEmployee para dentro da variável updateItem  passando o id do item selecionado como primeiro parâmetro e os novos dados do formulário
+      const updateItem = await updateEmployee(selectedItem.id, employeeData);
+
+      // Precisamos pegar o índice do item que estamos editando oara salvar no banco de dados e renderizar a tabela novamente.
+      const i = employees.indexOf(selectedItem);
+      employees[i] = updateItem;
+      renderData();
+
+      // Chamamos a função para selecionar o item novamente para corrigir o bug que limpava a marcação
+      selectItem(updateItem, elementList.children[i]);
+    } else {
+      const createdItem = await createEmployee(employeeData);
+      employees.push(createdItem);
+      renderData();
+      selectItem(createdItem, elementList.lastChild);
+      elementList.lastChild.scrollIntoView();
+    }
+  }
+}
+
+async function onDelete() {
+  if (selectedItem) {
+    await deleteEmployee(selectedItem.id);
+    const i = employees.indexOf(selectedItem);
+    employees.splice(i, 1);
+    renderData();
+    clearSelection();
+  }
 }
 
 function renderData() {
+  // Sempre que a tabela for renderizada, vamos limpar os dados para renderizar com as informações atualizadas.
+  elementList.innerHTML = "";
+
   for (const employee of employees) {
     //O método find() retorna o valor do primeiro elemento do array que satisfizer a função de teste provida.
     let role = roles.find((role) => role.id == employee.role_id);
@@ -86,7 +147,13 @@ function renderRoles() {
   }
 }
 
-function showError(errors) {
-  document.getElementById("errors").textContent = "Erro ao carregar dados.";
-  console.log(errors);
+function showError(message, errors) {
+  document.getElementById("errors").textContent = message;
+  if (errors) {
+    console.log(errors);
+  }
+}
+
+function clearError() {
+  document.getElementById("errors").textContent = "";
 }
